93715eaf6afc5449e687fcacfc59d1489256cead
SigneAnnaMaria <signe.annamaria@gmail.com>
2023-11-20 22:21:24 +0100

    project initialization, create react app and set up the view for the Stripe products

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ProductListing from './components/ProductListing'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <ProductListing />
+      </header>
+    </div>
+  );
+}
+
+export default App;
